1e87937acceb208737112be5d6a45158dc416fb5
Benjamin Vellacott <bvellacott@infusion.com>
2017-06-30 13:25:41 +0100

    added yieldAll method for testability, updated the readme and bumped the version

--- index.js
+++ index.js
@@ -1,56 +1,81 @@
-var undefined;
+function Undefined(){}; 
 
 function isPromise(val) {
   return !!val && typeof val === 'object' && typeof val.then === 'function' && typeof val.catch === 'function';
+}
+
+function argsToArray(_args) {
+  var args = [];
+  for(var i = 0; i < _args.length; i++)
+    args.push(_args[i]);
+  return args;
 }
 
 function async(gen) {
   if(typeof gen !== 'function')
     throw new Error('The argument to async must be a generator function');
 
-  return function() {
-    var args = [];
-    for(var i = 0; i < arguments.length; i++)
-      args.push(arguments[i]);
-
+  function Async() {
+    var args = argsToArray(arguments);
     return new Promise((resolve, reject) => {
-      tryCatchIteration(gen.apply(null, args), undefined, false, resolve, reject) 
+      tryCatchIteration(gen.apply(null, args), new Undefined(), false, resolve, reject) 
     });
   };
+
+// add testability
+  Async.yieldAll = function yieldAll() {
+    var args = argsToArray(arguments);
+    return new Promise((resolve, reject) => {
+      tryCatchIteration(gen.apply(null, args), new Undefined(), false, resolve, reject, []) 
+    });
+  }
+
+  return Async;
 }
 
-function tryCatchIteration(iter, prevVal, isError, resolve, reject) {
+function tryCatchIteration(iter, prevVal, isError, resolve, reject, results) {
   try {
-    runIteration(iter, prevVal, isError, resolve, reject); 
+    runIteration(iter, prevVal, isError, resolve, reject, results); 
   } catch(e) {
-    reject(e);
+    reject(genResolvable(e, results));
   }
 }
 
-function runIteration(iter, prevVal, isError, resolve, reject) {
+function runIteration(iter, prevVal, isError, resolve, reject, results) {
+  // if we want to collect all results and/or errors
+  if(!(prevVal instanceof Undefined) && typeof results !== 'undefined')
+    results.push(prevVal);
+
   var retVal = isError ? iter.throw(prevVal) : iter.next(prevVal);
+
   var done = retVal.done;
   var value = retVal.value;
 
-  if(isPromise(value)) {
-    if(done) {
-      return resolve(value);
-    }
+  if(!isPromise(value))
+    value = isError ? Promise.reject(value) : Promise.resolve(value);
 
-    value
-    .then(res => { 
-      tryCatchIteration(iter, res, false, resolve, reject);
-    })
-    .catch(err => { 
-      tryCatchIteration(iter, err, true, resolve, reject);
-    });
+  if(done) {
+    return value
+      .then(res => resolve(genResolvable(res, results)))
+      .catch(err => reject(genResolvable(err, results)));
   }
-  else {
-    if(done) {
-      return resolve(value);
+
+  value
+  .then(res => { 
+    tryCatchIteration(iter, res, false, resolve, reject, results);
+  })
+  .catch(err => { 
+    tryCatchIteration(iter, err, true, resolve, reject, results);
+  });
+}
+
+function genResolvable(value, results) {
+      // if all results were collected resolve them. Else resolve the return value of the async function
+    if(results) {
+      results.push(value);
+      return results;
     }
-    tryCatchIteration(iter, value, false, resolve, reject);
-  }
+    return value;
 }
 
 module.exports = async;
--- tests/all.js
+++ tests/all.js
@@ -52,9 +52,9 @@ var runResolves = async(function*(res1, res2, res3) {
   return results;
 });
 
-it('test error handling', function(done) {
+it('test error handling', done => {
   runErrors('error 1', 'error 2')
-  .then(res => done(new Error("shouldn't resolve")))
+  .then(res => done(new Error("shouldn't resolve: " + res)))
   .catch(errors => {
     assert.deepEqual(errors, ['error 1', 'error 2']);
     done();
@@ -62,8 +62,53 @@ it('test error handling', function(done) {
   .catch(err => done(err));
 });
 
-it('test value resolving', function(done) {
+it('test value resolving', done => {
   runResolves('hello 1', 'hello 2', 'no promise')
+  .then(results => {
+    assert.deepEqual(results, ['hello 1', 'hello 2', 'no promise']);
+    done();
+  })
+  .catch(err => done(err));
+});
+
+var runErrorsYA = async(function*(err1, err2, err3) {
+  try {
+    yield waitAndReject(err1);
+  } catch(e) {
+    console.error(e);
+  }
+
+  try {
+    yield throwImmediateError(err2);
+  } catch(e) {
+    console.error(e);
+  }
+
+  throw err3;
+});
+
+var runResolvesYA = async(function*(res1, res2, res3) {
+  yield waitAndResolve(res1);
+
+  yield res2;
+
+  return res3;
+});
+
+it('test yieldAll for errors', done => {
+  runErrorsYA.yieldAll('error 1', 'error 2', 'error 3')
+  .then(res => done(new Error("shouldn't resolve: " + res)))
+  .catch(errors => {
+    // only rejected promises and uncaught errors should be listed, that's why 'error 2' is not
+    // expected to show up
+    assert.deepEqual(errors, ['error 1', 'error 3']);
+    done();
+  })
+  .catch(err => done(err));
+});
+
+it('test yieldAll value resolving', done => {
+  runResolvesYA.yieldAll('hello 1', 'hello 2', 'no promise')
   .then(results => {
     assert.deepEqual(results, ['hello 1', 'hello 2', 'no promise']);
     done();
